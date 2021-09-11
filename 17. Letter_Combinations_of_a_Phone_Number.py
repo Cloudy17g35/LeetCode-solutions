71ae4dc84354077ba3f68e6bd7f2723fcfa318f7
Ksawery Lejczak <79964602+Cloudy17g35@users.noreply.github.com>
2021-09-11 10:40:09 +0200

    Update 17. Letter_Combinations_of_a_Phone_Number.py

--- 17. Letter_Combinations_of_a_Phone_Number.py
+++ 17. Letter_Combinations_of_a_Phone_Number.py
@@ -1,17 +1,16 @@
-class Solution:
-    def letterCombinations(self, digits: str) -> List[str]:
+class Solution(object):
+    def letterCombinations(self,  digits):
         map_table = {'2': 'abc', '3': 'def',
-                    '4': 'ghi', '5': 'jkl', 
+                    '4': 'ghi', '5': 'jkl',
                     '6': 'mno', '7': 'pqrs',
                     '8': 'tuv', '9': 'wxyz'}
-        result = []
         if not digits:
-            return result
+            return []
         if int(digits) < 10:
             return list(map_table[digits])
         else:
-            k = 1
-            while k != len(digits):
-                i = 0
-                for j in range(digits_length):
-                    
+            from itertools import product
+            letters = []
+        for element in digits:
+            letters.append(map_table[element])
+        return [''.join(element) for element in product(*letters)]
