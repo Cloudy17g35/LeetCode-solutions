3a653236b91880cb52e42097096eebaec192552e
Ksawery Lejczak <79964602+Cloudy17g35@users.noreply.github.com>
2021-12-27 13:36:42 +0100

    Create 1207. Unique Number of Occurrences.py

---
+++ 1207. Unique Number of Occurrences.py
@@ -0,0 +1,9 @@
+'''Given an array of integers arr, return true if the number 
+of occurrences of each value in the array is unique, or false otherwise.'''
+
+from collections import Counter
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        d = Counter(arr)
+        
+        return True if len(set(d.values())) == len(d.values()) else False
