7814d670c781facb67e92e6538e729ade43d8400
Ksawery Lejczak <79964602+Cloudy17g35@users.noreply.github.com>
2021-12-27 16:28:18 +0100

    Create 1185. Day of the Week.py

---
+++ 1185. Day of the Week.py
@@ -0,0 +1,13 @@
+'''Given a date, return the corresponding day of the week for that date.
+
+The input is given as three integers representing the day, month and year respectively.
+
+Return the answer as one of the following values {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}.'''
+
+from datetime import date
+
+class Solution:
+    def dayOfTheWeek(self, day: int, month: int, year: int) -> str:
+        d = date(year, month, day)
+        
+        return d.strftime('%A')
