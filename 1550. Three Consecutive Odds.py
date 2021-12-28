35e23561958a983121af5bc412746efc797735c8
Ksawery Lejczak <79964602+Cloudy17g35@users.noreply.github.com>
2021-12-28 12:07:46 +0100

    Create 1550. Three Consecutive Odds.py

---
+++ 1550. Three Consecutive Odds.py
@@ -0,0 +1,20 @@
+'''Given an integer array arr, return true if there are three consecutive odd numbers in the array. Otherwise, return false.'''
+
+
+class Solution:
+    def threeConsecutiveOdds(self, arr: List[int]) -> bool:
+
+
+        if len(arr) < 3:
+            return False
+
+        i, j, k = 0, 1, 2
+
+        while k != len(arr):
+            if (arr[i] % 2) and (arr[j] % 2) and (arr[k] % 2):
+                return True
+            i += 1
+            j += 1
+            k += 1
+
+        return False
