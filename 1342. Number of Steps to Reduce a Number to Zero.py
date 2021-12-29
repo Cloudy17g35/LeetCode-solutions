fff184058bd34f0ac89493669f62068e536f2fb5
Ksawery Lejczak <79964602+Cloudy17g35@users.noreply.github.com>
2021-12-29 12:27:26 +0100

    Create 1342. Number of Steps to Reduce a Number to Zero.py

---
+++ 1342. Number of Steps to Reduce a Number to Zero.py
@@ -0,0 +1,15 @@
+'''Given an integer num, return the number of steps to reduce it to zero.
+
+In one step, if the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.'''
+
+class Solution:
+    def numberOfSteps(self, num: int) -> int:
+        result = 0
+        while num:
+            if num % 2:
+                num -= 1
+            else:
+                num /= 2
+            result += 1
+
+        return result
