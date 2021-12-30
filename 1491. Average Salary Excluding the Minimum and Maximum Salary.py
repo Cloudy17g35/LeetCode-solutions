61574b52b9027a27d595696f6009cfefd43ac039
Ksawery Lejczak <79964602+Cloudy17g35@users.noreply.github.com>
2021-12-30 15:00:18 +0100

    Create 1491. Average Salary Excluding the Minimum and Maximum Salary.py

---
+++ 1491. Average Salary Excluding the Minimum and Maximum Salary.py
@@ -0,0 +1,10 @@
+'''You are given an array of unique integers salary where salary[i] is the salary of the ith employee.
+
+Return the average salary of employees excluding the minimum and maximum salary. Answers within 10-5 of the actual answer will be accepted.'''
+
+
+
+import numpy as np
+class Solution:
+    def average(self, salary: List[int]) -> float:
+        return np.mean(sorted(salary)[1:-1])
