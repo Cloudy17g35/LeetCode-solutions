0f47542ca6f122e83dae10075c279ae18106b818
Ksawery Lejczak <79964602+Cloudy17g35@users.noreply.github.com>
2022-01-17 20:45:04 +0100

    Create 1480. Running Sum of 1d Array.py

---
+++ 1480. Running Sum of 1d Array.py
@@ -0,0 +1,13 @@
+'''Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+Return the running sum of nums.
+
+'''
+
+
+from itertools import accumulate
+
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        return list(accumulate(nums))
