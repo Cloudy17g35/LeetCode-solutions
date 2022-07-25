9388a75619c354a3d158b52d66abbb4ce4067891
Ksawery Lejczak <79964602+Cloudy17g35@users.noreply.github.com>
2022-07-26 00:56:14 +0200

    Update 347. Top K Frequent Elements.py

--- 347. Top K Frequent Elements.py
+++ 347. Top K Frequent Elements.py
@@ -1,8 +1,17 @@
-from collections import Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        d = Counter(nums)
+        count = {}
+        freq = [[] for _ in range(len(nums) + 1)]
         
-        SortedCount = sorted(d.items() ,key=lambda x : x[1], reverse=True)[:k]
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
         
-        return [value for  value, count in SortedCount]
+        for n, c in count.items():
+            freq[c].append(n)
+            
+        res = []
+        for i in range(len(freq)- 1, 0, -1):
+            for v in freq[i]:
+                res.append(v)
+                if len(res) == k:
+                    return res
