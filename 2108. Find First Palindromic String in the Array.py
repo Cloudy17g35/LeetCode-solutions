a61c2970c2d68f60cf684f85a4da881038479043
Ksawery Lejczak <79964602+Cloudy17g35@users.noreply.github.com>
2024-02-13 20:39:11 +0100

    Update 2108. Find First Palindromic String in the Array.py

--- 2108. Find First Palindromic String in the Array.py
+++ 2108. Find First Palindromic String in the Array.py
@@ -1,6 +1,16 @@
 class Solution:
     def firstPalindrome(self, words: List[str]) -> str:
         for word in words:
-            if word == word[::-1]:
+            if is_palindrome(word):
                 return word
         return ''
+
+
+def is_palindrome(s:str) -> bool:
+    i, k = 0, len(s) - 1
+
+    while i <= k:
+        if s[i] != s[k]:
+            return False
+        i += 1; k -= 1
+    return True        
