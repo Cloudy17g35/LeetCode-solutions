62e31b83861764f4b871a3d6efa360f14677ccbd
Ksawery Lejczak <79964602+Cloudy17g35@users.noreply.github.com>
2024-11-10 14:24:17 +0100

    Create 2956. Find Common Elements Between Two Arrays.py

---
+++ 2956. Find Common Elements Between Two Arrays.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def findIntersectionValues(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        c1 = Counter(nums1)
+        c2 = Counter(nums2)
+        return [
+            sum([c1[c] for c in c2]),
+            sum([c2[c] for c in c1])
+        ]
